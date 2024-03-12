b2126df43c56fff551f39dfcade5cbbdee5d676d
Vaaaal25 <159541387+Vaaaal25@users.noreply.github.com>
2024-03-12 08:42:54 -0500

    12-03-2024

--- Assets/Scripts/PlataformaCaer.cs
+++ Assets/Scripts/PlataformaCaer.cs
@@ -4,38 +4,31 @@ using UnityEngine;
 
 public class PlataformaCaer : MonoBehaviour
 {
+  bool isFalling = false;
+  float downSpeed = 0;
+
+  void OnTriggerEnter(Collider collider)
+  {
+    if(collider.gameObject.name == "Player")
+    {
+      isFalling = true;
+      Destroy(gameObject, 10);
+    }
+  }
   
-    //[SerializeField] private float tiempoEspera;
-    //private Rigidbody rb;
-    //[SerializeField] private float velocidadRotacion;
-    //private Animator animator;
-    //private bool caida = false;
-
-    //void Start()
-    //{
-      // rb = GetComponent<Rigidbody>();
-      // animator = GetComponent<Animator>(); 
-    //}
-
-    //private void OnCollisionEnter(Collision other)
-   // {
-       // if (other.gameObject.CompareTag("Player"))
-       //{
-       //     StartCoroutine(Caida(other));
-        //}
-  //  }
-
-   // private IEnumerator Caida(Collision other)
-   // {
-      //  void return new WaitForSeconds(tiempoEspera);
-       // caida = true;
-       // Physics.IgnoreCollision(transform.GetComponent<collider>(), other.transform.GetComponent<collider>());
-       // rb.constraints = RigidbodyConstraints.None;
-       // rb.AddForce(new Vector3(0.1f, 0f, 0f));
-  //  }
+    
+    void Start()
+    {
+       
+    }
 
     void Update()
     {
-        
+        if (isFalling)
+        {
+          downSpeed += Time.deltaTime/200;
+          transform.position = new Vector3(transform.position.x, transform.position.y+downSpeed, transform.position.z);
+
+        }
     }
 }
