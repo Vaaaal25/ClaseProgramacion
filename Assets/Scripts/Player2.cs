5a7e6e47063f1ec20d05afc56ecd6ed0ee9567d1
bvasss <valerialpineda23@gmail.com>
2024-03-19 09:05:03 -0500

    19/03

---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public float velCaminar;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float movX = Input.GetAxis("Horizontal")*velCaminar;
+        float movY = Input.GetAxis("Vertical")*velCaminar;
+    }
+}
